e1e5f48ed3058f5dfc7080f806add5d20efabf14
hellyoshaqiqie17 <hellyoshaqiqie20@gmail.com>
2026-03-03 00:47:44 +0700

    terbaru

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -79,6 +79,19 @@ export default function AdminLayout({
           background: #fafafa;
           font-family: 'Montserrat', Arial, sans-serif;
           position: relative;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+        }
+
+        .admin-layout input,
+        .admin-layout textarea,
+        .admin-layout [contenteditable="true"] {
+          -webkit-user-select: text;
+          -moz-user-select: text;
+          -ms-user-select: text;
+          user-select: text;
         }
 
         .main-content {
